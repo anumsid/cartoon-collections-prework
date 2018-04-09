3eee95c3da083cf421d6ca7927dee9f922ac80d7
Anum Siddiqui <asiddiqui0692@gmail.com>
2018-04-09 23:34:06 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,8 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)# code an argument here
+  dwarves.each_with_index do |dwarve, index|
+      puts "#{index + 1}. #{dwarve}"
+  end
+
 end
 
 def summon_captain_planet# code an argument here
